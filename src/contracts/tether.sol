658df6de7f3bf1f8cea660855092e8fb237b79be
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-01-06 13:15:27 +0530

    tether smart contract shell

---
+++ src/contracts/tether.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Tether {
+    string public name = 'Tether';
+    string public symbol = 'USDT';
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8 public decimals = 18;
+}
