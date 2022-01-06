3488696bea1965f03af75821a246cbbd45018a2a
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-01-06 13:36:13 +0530

    change the tether symbol to mUSDT; mock USDT

--- src/contracts/tether.sol
+++ src/contracts/tether.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Tether {
     string public name = 'Tether';
-    string public symbol = 'USDT';
+    string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
 }
