85be9592e33341bc7a083e453a81c7f45bb8316c
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-01-07 17:55:48 +0530

    third party transfer function

--- src/contracts/tether.sol
+++ src/contracts/tether.sol
@@ -12,7 +12,6 @@ contract Tether {
         address indexed _to,
         uint _value
     );
-
     event Approve(
         address indexed _owner,
         address indexed _spender,
@@ -20,6 +19,7 @@ contract Tether {
     );
 
     mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
@@ -27,10 +27,23 @@ contract Tether {
 
     function transfer(address _to, uint _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+
         return true;
     }
 
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[msg.sender][_from] -= _value;
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
