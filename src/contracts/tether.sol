bd39acd792d10e87d7136f70b38e4b93bcd00c59
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-01-07 17:13:21 +0530

    add basic functionalities of tether

--- src/contracts/tether.sol
+++ src/contracts/tether.sol
@@ -6,4 +6,31 @@ contract Tether {
     string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
 }
