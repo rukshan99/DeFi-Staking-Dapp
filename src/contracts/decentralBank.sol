5d176434aaa4e5eda009e6f9f6a61befb7769217
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-01-09 04:07:03 +0530

    update decentral bank contract

--- src/contracts/decentralBank.sol
+++ src/contracts/decentralBank.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+import './reward.sol';
+import './tether.sol';
+
 contract DecentralBank {
     string public name = 'Decentral Bank';
     address public owner;
+    Tether public tether;
+    Reward public reward;
+
+    constructor(Reward _reward, Tether _tether) {
+        reward = _reward;
+        tether = _tether;
+    }
 }
