6c6741cee927c1b42fd27bdd6101dcd318d2992d
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-02-10 18:37:39 +0530

    warning fix and final setstates

--- src/components/App.js
+++ src/components/App.js
@@ -42,6 +42,7 @@ class App extends Component {
     async loadBlockchainData() {
         const web3 = window.web3;
         const accounts = await web3.eth.getAccounts();
+        this.setState({ account: accounts[0] || '0x023e333ca3' });
         const networkID = await web3.eth.net.getId();
 
         // Load Tether contract
@@ -49,7 +50,7 @@ class App extends Component {
         let tether = {};
         let tetherBalance = 0;
         if(tetherData) {
-            tether = {... new web3.eth.Contract(Tether.abi, tetherData.address)};
+            tether = {...new web3.eth.Contract(Tether.abi, tetherData.address)};
             tetherBalance = await tether.methods.balanceOf(this.state.account).call();
         } else {
             window.alert('ERROR: no Tether(mUSDT) contract detected.');
@@ -60,7 +61,7 @@ class App extends Component {
         let reward = {};
         let rewardBalance = 0;
         if(rewardData) {
-            reward = {... new web3.eth.Contract(Reward.abi, rewardData.address)};
+            reward = {...new web3.eth.Contract(Reward.abi, rewardData.address)};
             rewardBalance = await reward.methods.balanceOf(this.state.account).call();
         } else {
             window.alert('ERROR: no Reward(RWD) contract detected.');
@@ -71,20 +72,20 @@ class App extends Component {
         let decentralBank = {};
         let stakingBalance = 0;
         if(decentralBankData) {
-            decentralBank = {... new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)};
+            decentralBank = {...new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)};
             stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call();
         } else {
             window.alert('ERROR: no DecentralBank contract detected.');
         }
 
         this.setState({
-            account: accounts[0] || '0x023e333ca3',
             tether,
             reward,
             decentralBank,
             tetherBalance: tetherBalance.toString(),
             rewardBalance: rewardBalance.toString(),
-            stakingBalance: stakingBalance.toString()
+            stakingBalance: stakingBalance.toString(),
+            isLoading: false
         });
     }
 
@@ -92,9 +93,6 @@ class App extends Component {
         return (
             <div>
                 <Navbar account={this.state.account} />
-                <div className='text-center'>
-                    <h1></h1>
-                </div>
             </div>
         )
     }
