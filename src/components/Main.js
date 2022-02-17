b8bf38b529b1362375347ec319836050167d2e94
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-02-17 21:55:41 +0530

    link submit of main component with staking tokens function

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,30 +8,35 @@ class Main extends Component {
             <div id='content' className='mt-3'>
                 <table className='table text-muted text-center'>
                     <thead>
-                        <tr style={{color:'white'}}>
+                        <tr style={{ color: 'white' }}>
                             <th scope='col'>Staking Balance</th>
                             <th scope='col'>Reward Balance</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr style={{color:'white'}}>
+                        <tr style={{ color: 'white' }}>
                             <td>{window.web3.utils.fromWei(this.props.stakingBalance, 'Ether')} mUSDT</td>
                             <td>{window.web3.utils.fromWei(this.props.rewardBalance, 'Ether')} RWD</td>
                         </tr>
                     </tbody>
                 </table>
-                <div className='card mb-2' style={{opacity:'0.9'}}>
-                    <form className='mb-3'>
-                        <div style={{borderSpacing:'0 1em'}}>
-                            <label className='float-left' style={{marginLeft:'15px'}}><b>Stake Tokens</b></label>
-                            <span className='float-right' style={{marginRight:'8px'}}>
+                <div className='card mb-2' style={{ opacity: '0.9' }}>
+                    <form className='mb-3' onSubmit={(event) => {
+                        event.preventDefault();
+                        let amount = this.input.value.toString();
+                        amount = window.web3.utils.toWei(amount, 'Ether');
+                        this.props.stakeTokens(amount);
+                    }}>
+                        <div style={{ borderSpacing: '0 1em' }}>
+                            <label className='float-left' style={{ marginLeft: '15px' }}><b>Stake Tokens</b></label>
+                            <span className='float-right' style={{ marginRight: '8px' }}>
                                 Balance: {window.web3.utils.fromWei(this.props.tetherBalance, 'Ether')}
                             </span>
                             <div className='input-group mb-4'>
-                                <input type='text' placeholder='0' required />
+                                <input type='text' placeholder='0' ref={(input) => { this.input = input }} required />
                                 <div className='input-group-open'>
                                     <div className='input-group-text'>
-                                        <img src={tether} alt='mtether' height='32'/>
+                                        <img src={tether} alt='mtether' height='32' />
                                         &nbsp;&nbsp;&nbsp; mUSDT
                                     </div>
                                 </div>
@@ -40,7 +45,7 @@ class Main extends Component {
                         </div>
                     </form>
                     <button className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
-                    <div className='card-body text-center' style={{color:'blue'}}>
+                    <div className='card-body text-center' style={{ color: 'blue' }}>
                         AIRDROP
                     </div>
                 </div>
